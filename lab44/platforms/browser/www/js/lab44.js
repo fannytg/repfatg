598bb2f416a46498dc6fd8b24db58f9e6cb066cb
fannytg <fannytg@gmail.com>
2015-05-29 19:47:08 -0500

    message

---
+++ lab44/platforms/browser/www/js/lab44.js
@@ -0,0 +1,18 @@
+(function(){
+document.addEventListener("deviceready", onDeviceReady, false);
+
+    // device APIs are available
+    //
+function onDeviceReady() {
+        var element = document.getElementById('deviceProperties');
+        element.innerHTML = 'Device Name: '     + device.name     + '<br />' +
+                            'Device Cordova: '  + device.cordova  + '<br />' +
+                            'Device Platform: ' + device.platform + '<br />' +
+                            'Device UUID: '     + device.uuid     + '<br />' +
+                            'Device Model: '    + device.model    + '<br />' +
+                            'Device Version: '  + device.version  + '<br />';
+}
+
+//$('#deviceProperties').append("");
+
+})();
